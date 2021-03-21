71ab148fa54df7031127de72015b656f35036437
NewtRZ <candojohn76@gmail.com>
2021-03-21 08:28:56 +0800

    update

---
+++ src/main/java/com/mycompany/myfirstjavaapplication1/Product.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myfirstjavaapplication1;
+
+/**
+ *
+ * @author John Frederick T Cando
+ */
+
+public class Product {
+    
+    private int Code;
+    
+    public Product(int getCode){
+        this.Code = Code;
+    }
+    public String getName (){
+           this.Code = Code;
+          
+    }
+    public float getCost (){
+        return Math.PI * Math.pov(getCode(), 2);
+    }
+    public int getStock();
+    
+    
+    
+  
+ 
+   
+    
+            
+}
